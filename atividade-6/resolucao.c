f2cdfca6770d4759786e4015ff0848d185d20985
Carlos12328 <cadu.eduardo12328@gmail.com>
2025-03-12 17:17:59 -0300

    adicionando resoluções

---
+++ atividade-6/resolucao.c
@@ -0,0 +1,91 @@
+#include <stdio.h> // Funções de entrada e saída de dados
+
+int main() {
+    int i;
+
+    i = 0;
+
+    // While verifica primeiro, depois executa
+    while (i < 10) {
+        printf("while: %d\n", i);
+        i++;
+    }
+
+    // Do executa primeiro, depois verifica
+    do {
+        printf("do: %d\n", i);
+        i++;
+    } while (i < 10);
+
+    return 0;
+}
+
+/*
+a) Qual a saída do programa acima?
+
+    a saída do programa acima será:
+    while: 0
+    while: 1
+    while: 2
+    while: 3
+    while: 4
+    while: 5
+    while: 6
+    while: 7
+    while: 8
+    while: 9
+    do: 10
+
+b) Qual a diferença entre o while e do do.while?
+
+    o while simples ele só ira executar o bloco de codigo após verificar a condição
+    o do while irá executar uma vez e depois que vai verificar a condição
+
+c) Como fica a implementação do bloco
+
+    implementação do bloco while:
+
+        int i = 0;
+
+        while (i < 10) {
+            printf("while: %d\n", i);
+            i++;
+        }
+
+    implementação do bloco do-while:
+
+        do {
+            printf("do: %d\n", i);
+            i++;
+        } while (i < 10);
+
+d) Qual a diferença entre while e for? Quando usar um ou outro?
+
+    while é usado quando o número de repetições não é conhecido antes e depende de uma condição que pode ser alterada
+    for é quando o o número de repetições é conhecido antes ou é definido por um contador
+
+    Use While: 
+
+    Quando a execução depender de uma condição dinâmica.
+
+    Exemplo:
+
+    c
+    int i = 0;
+    while (i < 10) {
+        printf("%d\n", i);
+        i++;
+    }
+        
+    Use for:
+
+    Quando o loop tiver uma estrutura fixa (inicialização, condição, incremento/decremento).
+
+    Exemplo:
+
+    c
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", i);
+    }
+
+*/
